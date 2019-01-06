f31ee320f5563311031ccb7370ef13b4c7c713ad
Yao Adzaku <yao.adzaku@gmail.com>
2019-01-06 03:04:13 +0000

    add partner application feedback page

--- sendhut/partners/urls.py
+++ sendhut/partners/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import PartnerApplication
+from .views import PartnerApplication, PartnerApplicationSuccess
 
 
 urlpatterns = [
     url(r'^$', PartnerApplication.as_view(), name='apply'),
+    url(r'^feedback$', PartnerApplicationSuccess.as_view(),
+        name='application_feedback'),
 ]
--- sendhut/partners/views.py
+++ sendhut/partners/views.py
@@ -1,5 +1,7 @@
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 from django.contrib import messages
+from django.urls import reverse
 from .forms import PartnerForm
 
 # Create your views here.
@@ -10,9 +12,14 @@ from .forms import PartnerForm
 class PartnerApplication(FormView):
     template_name = 'partners/apply.html'
     form_class = PartnerForm
-    success_url = '/partners'
+    success_url = '/partners/feedback'
 
     def form_valid(self, form):
         # TODO(yao): send email/sms
-        messages.info(self.request, "Thank you. We'll contact you shortly")
+        messages.success(self.request, "Thank you. We'll contact you shortly")
         return super().form_valid(form)
+
+
+class PartnerApplicationSuccess(TemplateView):
+
+    template_name = "partners/application_feedback.html"
